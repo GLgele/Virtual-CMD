37b79bd96f2f9617bd056a87fa70ce30b76ee75d
GLgele <ai_minecraft@163.com>
2020-05-30 11:35:24 +0800

    add .h FILE

---
+++ V-CMD/vcmd.h
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<Windows.h>
+using namespace std;
+#ifndef _VCMD_H_
+#define _VCMD_H_
+
+void wait(int sec)
+{
+	if(sec == 1)
+	{
+		system("ping localhost -n 2 >nul");
+		return;
+	}
+	else
+	if(sec == 3)
+	{
+		system("ping localhost -n 3 >nul");
+		return;
+	}
+	else
+	if(sec == 5)
+	{
+		system("ping localhost -n 5 >nul");
+		return;
+	}
+	else
+	{
+		system("msg * 错误的函数调用：使用了错误的参数于wait()");
+		return;
+	}
+}
+
+void start()
+{
+	system("cls");
+	system("pause");
+	system("cls");
+	cout<<"DreamBorderX VirtualCMD [版本 1.0.0]\n (c) 2020 DreamBorderX & GLgele。保留所有权利。 \n";
+	while(1 == 1)
+	{
+		cout<<"\nVcmd>";
+		string stri;
+		cin>>stri;
+		if(stri == "exit")
+		{
+			system("cls");
+			exit(0);
+		}
+		else if(stri == "help")
+		{
+			cout<<"该模拟CMD由GLgele编写。指令在Command.txt里。\n";
+		}
+		else if(stri == "lincense")
+		{
+			cout<<"请查看lincense文件。\n";
+		}
+		else if(stri == "credits")
+		{
+			cout<<"Dream Border X \n"<<"GLgele \n";
+		}
+		else if(stri == "copyright")
+		{
+			cout<<"(c) 2020 DreamBorderX & GLgele。保留所有权利。 \n";
+		}
+		else if(stri == "exit")
+		{
+			exit(0);
+		}
+		else if(stri == "clear")
+		{
+			system("cls");
+		}
+		else
+		{
+			cout<<"不存在的命令，请输入help查看帮助！\n";
+		}
+		
+	}
+}
+
+#endif
