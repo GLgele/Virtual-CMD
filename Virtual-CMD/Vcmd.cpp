d33509fe328279fc15952a4e50419a6798a49ae4
GLgele <GLgele@users.noreply.github.com>
2020-06-13 21:25:46 +0800

    update

--- Virtual-CMD/Vcmd.cpp
+++ Virtual-CMD/Vcmd.cpp
@@ -5,6 +5,9 @@
 #include "vcmd.h"
 using namespace std;
 string stri = "";
+const string ver = "vcmdH.h";
+const string Ver = "vcmd.h";
+const string version = "1.0.0"
 int main()
 {
 	int temp = 0;
@@ -36,15 +39,18 @@ int main()
 	else
 	{
 		system("cls");
-		cout<<"[控制台]正在启动...\n";
+		cout << "[控制台]正在启动...\n";
 		wait(3);
- 	    cout<<"[控制台]正在读取数据...\n";
+		cout << "[控制台]正在读取数据...\n";
 		wait(3);
-		cout<<"[控制台]正在注册注册表...\n";
+		cout << "[控制台]正在注册注册表...\n";
 		wait(3);
 		system("msg * -time:3 注册成功 !");
 		wait(3);
-		cout<<"[控制台]V-CMD已启动，以后可以通过faststart唤醒\n";
+		cout << "[控制台]核心：" << ver << endl;
+		cout << "[控制台]库：" << Ver << endl;
+		cout << "[控制台]版本：" << version;
+		cout << "[控制台]V-CMD已启动，以后可以通过faststart唤醒\n";
 		start();
 	}
 }
