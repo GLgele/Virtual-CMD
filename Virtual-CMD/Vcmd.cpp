446d22186441f91fb9ac9a54fc4ffd49c93217dc
GLgele <ai_minecraft@163.com>
2020-10-07 20:16:26 +0800

    update

--- Virtual-CMD/Vcmd.cpp
+++ Virtual-CMD/Vcmd.cpp
@@ -45,12 +45,13 @@ int main()
 		wait(3);
 		cout << "[控制台]正在注册注册表...\n";
 		wait(3);
-		system("msg * -time:3 注册成功 !");
+		MessageBox(NULL,TEXT("注册成功！"),TEXT("regedit"),MB_OK|MB_ICONINFORMATION);
 		wait(3);
 		cout << "[控制台]核心：" << ver << endl;
 		cout << "[控制台]库：" << Ver << endl;
-		cout << "[控制台]版本：" << version;
+		cout << "[控制台]版本：" << version << endl;
 		cout << "[控制台]V-CMD已启动，以后可以通过faststart唤醒\n";
+		system("pause");
 		start();
 	}
 }
